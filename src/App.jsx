d3576cb737b25d8748ddf9af11aa1e6e6db0a90a
Dominic Tillison <22tilldj@gmail.com>
2025-05-01 17:05:34 -0400

    added today's orders view

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import { ViewOrder } from "./components/viewOrder/ViewOrder.jsx";
 
 function App() {
-  return <div className="App">Shepherd's Pies</div>;
+
+  return (
+  <Routes>
+    <Route path={"/"}>
+      <Route index element={<div>Welcome</div>} />
+      <Route path="orders" element={<ViewOrder />} />
+    </Route>
+  </Routes>
+  )
 }
 
 export default App;
---
+++ src/components/viewOrder/ViewOrder.jsx
@@ -0,0 +1,81 @@
+import { useEffect, useState } from "react";
+import { getOrdersWithCustomer } from "../../services/getOrders.jsx";
+import { use } from "react";
+
+export const ViewOrder = () => {
+  let orderMonthArr = []
+  const [orders, setOrders] = useState([]);
+  const [orderDate, setOrderDate] = useState([]);
+  const [month, setMonth] = useState(0)
+
+  useEffect(() => {
+    getOrdersWithCustomer().then(setOrders);
+  }, []);
+
+  useEffect(() => {}, []);
+
+  const isDate = (dateString) => {
+    const orderDate = new Date(dateString);
+    const today = new Date();
+
+    return (
+      orderDate.getFullYear() === today.getFullYear() &&
+      orderDate.getMonth() === today.getMonth() &&
+      orderDate.getDate() === today.getDate()
+    );
+  };
+
+  useEffect(() => {
+    orders.map((order) => {
+      const date = new Date(order.orderTime);
+      const orderMonthInt = date.getUTCMonth() + 1;
+      orderMonthArr.push(orderMonthInt)
+      setOrderDate(orderMonthArr)
+    });
+  }, [orders])
+ 
+  
+
+
+  return (
+    <div>
+      <h2>Today's Orders</h2>
+      <select onChange={(e) => setMonth(parseInt(e.target.value))}>
+        <option>Month</option>
+          {[
+            ...new Set(
+              orders.map((order) => new Date(order.orderTime).getUTCMonth() + 1)
+            ),
+          ]
+            .sort((a, b) => a - b)
+            .map((month, index) => {
+              return (
+                <option key={index} value={month}>
+                  {" "}
+                  {month}
+                </option>
+              );
+            })}
+      </select>
+      {orders
+        .filter((order) => month > 0 ? new Date(order.orderTime).getUTCMonth() + 1 === month 
+        : isDate(order.orderTime))
+        .map((order) => (
+          <fieldset key={order.id} style={{ marginBottom: "1rem" }}>
+            <div>
+              <h3>Order Time: {order.orderTime}</h3>
+            </div>
+            <div>
+              <h3>Order ID: {order.id}</h3>
+            </div>
+            <div>
+              <h3>Customer: {order.customer?.name}</h3>
+            </div>
+            <div>
+              <h3>Status: {order.status}</h3>
+            </div>
+          </fieldset>
+        ))}
+    </div>
+  );
+};
